5372679f3477bebc3a3ef47ceac326c8b8a9320f
Collene De Silva <collene.desilva@gmail.com>
2024-02-14 20:15:48 +0800

    feat: move javascript into a separate file

---
+++ 01 - Inception/namaste-react-01/App.js
@@ -0,0 +1,9 @@
+// Creation of our element using React
+const heading = React.createElement("h1", {}, "Hello World from React!");
+
+// React needs a root to work on, creating the root using our div element earlier.
+// Everything that we will render, we will render inside this root.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Rendering our elements in our root
+root.render(heading);
