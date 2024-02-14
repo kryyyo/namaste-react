7235e6425021cd6f7f10e0b58eb6e929a55176f3
Collene De Silva <collene.desilva@gmail.com>
2024-02-14 20:35:04 +0800

    feat: creating nested elements

--- 01 - Inception/namaste-react-01/App.js
+++ 01 - Inception/namaste-react-01/App.js
@@ -1,15 +1,47 @@
+/**
+ * 
+ * <div id="parent">
+ *    <div id="child">
+ *      <h1>I'm h1 tag</h1>
+ *    </div>
+ * 
+ * </div>
+ * 
+ * ReactElement(Object) => HTML(Browser Understands)
+ * 
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement(
+      "h1",
+      {},
+      "I'm h1 tag"
+    )
+  )
+);
+
+/**
+ * 
 // Creation of our element using React
 const heading = React.createElement(
   "h1",
-  {id: "heading"},                    // second args is for the attributes of a tag
+  { id: "heading" },                    // second args is for the attributes of a tag
   "Hello World from React!"
 );
 
 console.log(heading); // not an actual h1 tag yet! this is an object
+ */
+
+console.log(parent);
 
 // React needs a root to work on, creating the root using our div element earlier.
 // Everything that we will render, we will render inside this root.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Rendering our elements in our root
-root.render(heading);
+// Rendering our elements in our root. This is the one who is converting the object to element.
+root.render(parent);
