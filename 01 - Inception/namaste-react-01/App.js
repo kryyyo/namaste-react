81bde8b0db79c8c2c8f113bb8a2689f6eaa0c6d2
Collene De Silva <collene.desilva@gmail.com>
2024-02-14 20:17:23 +0800

    feat: added attributes to the heading element

--- 01 - Inception/namaste-react-01/App.js
+++ 01 - Inception/namaste-react-01/App.js
@@ -1,5 +1,9 @@
 // Creation of our element using React
-const heading = React.createElement("h1", {}, "Hello World from React!");
+const heading = React.createElement(
+  "h1",
+  {id: "heading"},                    // second args is for the attributes of a tag
+  "Hello World from React!"
+);
 
 // React needs a root to work on, creating the root using our div element earlier.
 // Everything that we will render, we will render inside this root.
