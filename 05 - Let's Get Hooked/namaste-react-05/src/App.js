09df79cc7ae2d6464236b9e2f68c8b022d37095b
Collene De Silva <collene.desilva@gmail.com>
2024-02-29 17:20:23 +0800

    feat: add src folder and move js files there

---
+++ 05 - Let's Get Hooked/namaste-react-05/src/App.js
@@ -0,0 +1,117 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      {/* Logo */}
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://graphicsfamily.com/wp-content/uploads/edd/2021/06/Editable-Photoshop-Food-Logo-Design-PNG-Transparent.png"
+          alt="logo-of-the-app"
+        />
+      </div>
+      {/* Nav Items */}
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+// Inline Style in JSX
+/**
+ * const styleCard = {
+ *    backgroundColor: "#f0f0f0",
+ * }
+ */
+
+const resList = [
+  {
+    data: {
+      id: 1,
+      resName: "Meghana Foods",
+      cuisine: ["Biryani", "North Indian", "Asian"],
+      rating: 4.4,
+      deliveryTime: 38,
+      costForTwo: 40000,
+      cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf"
+    },
+  },
+  {
+    data: {
+      id: 2,
+      resName: "KFC",
+      cuisine: ["American", "Fries"],
+      rating: 4.8,
+      deliveryTime: 40,
+      costForTwo: 50000,
+      cloudinaryImageId: "03501c33ecb3a3105124441e541e6fe4"
+    },
+  }
+]
+
+const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  const { cloudinaryImageId, resName, cuisine, rating, deliveryTime, costForTwo } = resData?.data;
+  
+  return (
+    // Inline Style in JSX
+    <div
+      className="res-card"
+      // style={styleCard}
+      style={{
+        backgroundColor: "#f0f0f0",
+      }}
+    > 
+      <img
+        className="res-logo"
+        alt="meghana-foods-logo"
+        height="200px"
+        src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+      />
+      <h3>{resName}</h3>
+      <h4>{cuisine.join(", ")}</h4>
+      <h4>{rating} stars</h4>
+      <h4>{deliveryTime} minutes</h4>
+      <h4>${costForTwo / 100} FOR TWO</h4>
+    </div>
+  )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      {/* Search */}
+      <div className="search">
+        Search
+      </div>
+      {/* Restaurant Container */}
+      <div className="res-container">
+        {resList.map(restaurant => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+      {/* Footer */}
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
---
+++ 05 - Let's Get Hooked/notes.md
@@ -0,0 +1,3 @@
+Best practice
+1. separate files for separate components
+2. create `src` folder (convention) and put App.js there (and future components)
