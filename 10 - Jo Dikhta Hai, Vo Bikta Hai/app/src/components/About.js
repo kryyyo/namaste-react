a9c7bd61418a6991761383b53de35c356b345646
Collene De Silva <collene.desilva@gmail.com>
2024-08-20 07:33:51 +0800

    feat: - different ways to add styles - ui libraries examples

---
+++ 10 - Jo Dikhta Hai, Vo Bikta Hai/app/src/components/About.js
@@ -0,0 +1,47 @@
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props)
+
+    // console.log("Parent Constructor")
+  }
+
+  componentDidMount() {
+    // console.log("Parent Component Did Mount")
+  }
+
+  render() {
+    // console.log("Parent Render")
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>This is Namaste React Web Series</h2>
+        <UserClass
+          name={"Akshay Saini (class)"}
+          location={"Dehradun (class)"}
+        />
+        <UserClass
+          name={"Elon Musk"}
+          location={"US"}
+        />
+      </div>
+    )
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About</h1>
+//       <h2>This is Namaste React Web Series</h2>
+//       <UserClass
+//         name={"Akshay Saini (class)"}
+//         location={"Dehradun (class)"}
+//       />
+//     </div>
+//   )
+// }
+
+export default About;
---
+++ 10 - Jo Dikhta Hai, Vo Bikta Hai/notes.md
@@ -0,0 +1,17 @@
+Normal CSS
+- index.css, className attribute
+
+SASS / SCSS
+- like writing CSS with superpowers
+- not used in industry level
+- not scalable
+
+Styled Components
+- https://styled-components.com
+
+UI Libraries Examples
+- MUI
+- Bootstrap
+- Tailwind CSS
+- Chakra UI
+- Ant Design
