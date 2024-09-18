34dc02e7c22e20b3ff47b929657074e73a60c94d
Collene De Silva <collene.desilva@gmail.com>
2024-09-18 16:37:53 +0800

    feat: controlled and uncontrolled components

--- 11 - Data is the New Oil/app/src/components/RestaurantMenu.js
+++ 11 - Data is the New Oil/app/src/components/RestaurantMenu.js
@@ -1,29 +1,44 @@
 import Shimmer from "./Shimmer";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 // Single Responsibility of Restaurant Menu: Display the Data
 const RestaurantMenu = () => {
   const { resId } = useParams();            // SR: Get the Id from the Params
   const resInfo = useRestaurantMenu(resId); // SR: Fetch The Data
 
+  const [showIndex, setShowIndex] = useState(null);
+
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info;
-  const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+  const { itemCards } = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+
+  const categories = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(c => c.card?.card?.['@type'] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory");
   
   return (
-    <div className="menu">
-      <h1>{name}</h1>
-      <h3>{cuisines.join(", ")} - {costForTwoMessage}</h3>
-      <h3>{costForTwoMessage}</h3>
-      <ul>
-        {itemCards.map(item => (
-          <li key={item?.card?.info?.id}>
-            {item?.card?.info?.name} - Rs.{item?.card?.info?.price / 100}
-          </li>
-        ))}
-      </ul>
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <h3 className="font-bold text-lg">{cuisines.join(", ")} - {costForTwoMessage}</h3>
+      {/* Categories Accordion */}
+      {categories.map((category, index) => {
+        return (
+          // controlled component
+        <RestaurantCategory
+          key={category.card.card.title}
+          data={category?.card?.card}
+          showItems={index === showIndex}
+          setShowIndex={() => {
+            if (index === showIndex) {
+              setShowIndex(null)
+            } else {
+              setShowIndex(index)
+            }
+          }}
+        />)
+      })}
     </div>
   )
 }
--- 11 - Data is the New Oil/notes.md
+++ 11 - Data is the New Oil/notes.md
@@ -1,3 +1,17 @@
 # Higher Order Component
 - a function that takes a component and returns a component
 - acts like an *enhancer* for a component
+
+# 2 Layers of React
+- UI Layer 
+- Data Layer
+
+
+# Controlled Vs. Uncontrolled Component
+- if it has its own state, it is an uncontrolled component
+- if other component is controlling it, it is controlled component
+
+# Lifting State Up
+https://legacy.reactjs.org/docs/lifting-state-up.html
+
+# Props Drilling
