cc424da61b6cbc5e0248969a89bbc75800269d89
Collene De Silva <collene.desilva@gmail.com>
2024-09-23 15:50:39 +0800

    feat: build store

--- 12 - Let's Build Our Store/app/src/App.js
+++ 12 - Let's Build Our Store/app/src/App.js
@@ -8,6 +8,8 @@ import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import UserContext from "./utils/UserContext";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 // import Grocery from "./components/Grocery";
 
 // Chunking
@@ -31,13 +33,14 @@ const AppLayout = () => {
   }, [])
 
   return (
-    <UserContext.Provider value={{ loggedInUser: username, setUsername }}>
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
-    
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: username, setUsername }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   )
 };
 
