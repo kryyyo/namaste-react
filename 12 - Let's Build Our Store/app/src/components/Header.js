6f05853bd2b552526575af954b66025ed6fc7844
Collene De Silva <collene.desilva@gmail.com>
2024-09-23 16:25:24 +0800

    feat: read using selector

--- 12 - Let's Build Our Store/app/src/components/Header.js
+++ 12 - Let's Build Our Store/app/src/components/Header.js
@@ -3,6 +3,7 @@ import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 
 const Header = () => {
   // on rerender, buttonNameReact will be a NEW variable that is why even if it is declared as const, it is modifiable
@@ -12,6 +13,10 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   const { loggedInUser } = useContext(UserContext);
+
+  // Subscribing to the store using a Selector
+  const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems)
 
   return (
     <div className="flex justify-between bg-pink-100 shadow-xl px-4 sm:bg-yellow-200 lg:bg-green-200">
@@ -49,7 +54,7 @@ const Header = () => {
               Grocery
             </Link>
           </li>
-          <li className="px-4">Cart</li>
+          <li className="px-4 font-bold text-xl">Cart ({cartItems.length} items)</li>
           <button
             className="login"
             onClick={() => {
