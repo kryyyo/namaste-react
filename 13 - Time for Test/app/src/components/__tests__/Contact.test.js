178188cb0433610b56789a1bb10c0da5c1fa8ff4
Collene De Silva <collene.desilva@gmail.com>
2024-10-01 14:49:18 +0800

    feat: basics of testing

--- 13 - Time for Test/app/src/components/__tests__/Contact.test.js
+++ 13 - Time for Test/app/src/components/__tests__/Contact.test.js
@@ -9,4 +9,33 @@ test("Should load contact us component", () => {
 
   // Assertion
   expect(heading).toBeInTheDocument();
-})
+});
+
+test("Should load button inside contact us component", () => {
+  render(<Contact />);
+
+  const button = screen.getByText("Submit");
+
+  // Assertion
+  expect(button).toBeInTheDocument();
+});
+
+test("Should load input name inside contact us component", () => {
+  render(<Contact />);
+
+  const inputName = screen.getByPlaceholderText("name");
+
+  // Assertion
+  expect(inputName).toBeInTheDocument();
+});
+
+test("Should load 2 input boxes inside contact us component", () => {
+  // Rendering
+  render(<Contact />);
+
+  // Querying
+  const inputBoxes = screen.getAllByRole("textbox"); // returns React Element Component / JSX Element
+
+  // Assertion
+  expect(inputBoxes.length).toBe(2);
+});
