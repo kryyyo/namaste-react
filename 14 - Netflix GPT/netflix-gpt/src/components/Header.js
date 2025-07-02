6b0aedb1fd97550216d764900ae2781d94c5e135
kryo <collene.desilva@gmail.com>
2025-07-02 08:40:26 +0800

    feat: - add logo and background

---
+++ 14 - Netflix GPT/netflix-gpt/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <div className='absolute px-8 py-2 bg-gradient-to-b from-black'>
+      <img
+        className='w-44'
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Logonetflix.png/960px-Logonetflix.png'
+        alt='logo'
+      />
+    </div>
+  );
+};
+export default Header;
---
+++ 14 - Netflix GPT/netflix-gpt/src/components/Login.js
@@ -0,0 +1,19 @@
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div
+      className='h-screen bg-cover bg-center'
+      style={{
+        backgroundImage:
+          "url('https://wallpapers.com/images/high/netflix-background-gs7hjuwvv2g0e9fj.webp')",
+      }}
+    >
+      <div>
+        <Header />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
